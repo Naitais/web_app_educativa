db6e09a6e03a026dd861a655b1bf2d4ad466362e
Naitais <enzo.f9849@gmail.com>
2024-09-17 12:08:29 -0300

    commit inicial del esqueleto del proyecto con spring

---
+++ web_app/src/main/java/proyecto/web_app_educativa/WebAppEducativaApplication.java
@@ -0,0 +1,13 @@
+package proyecto.web_app_educativa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppEducativaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppEducativaApplication.class, args);
+	}
+
+}
